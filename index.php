2ab4aa2d2fef063a28dfa8b05d32a136039cb8bb
mysugardaddy <72257279+mysugardaddy@users.noreply.github.com>
2021-08-05 13:51:22 +0100

    Trial

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="main.css">
+	<title>Simulateur Bac - Accueil</title>
+</head>
 <body>
-<h1>Hello World</h1>
-<p>I'm hosted with GitHub Pages.</p>
+<h1>choisissez votre voie.</h1>
+<a href="generale.php">→ générale.</a>
+<a href="techno.php">→ technologique.</a>
 </body>
 </html>
